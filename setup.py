79ca4edd408d0d3ac7488eca1d2937cfd841f609
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2021-11-21 22:50:50 -0300

    Add travis yml

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    README = fh.read()
+
 setup(
     name="fpylib",
     packages=find_packages(include=["fpylib", "fpylib.irange", "fpylib.functors"]),
     version="0.1.0",
     description="This is a library to do functional programming in Python.",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    url="https://github.com/FabianVegaA/fpy_lib",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     author="Fabián Vega",
     license="MIT",
     install_requires=[],
