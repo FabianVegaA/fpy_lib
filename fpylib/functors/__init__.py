51c29a891b436c07472504996504a9cebe4dfbb1
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2021-11-21 23:46:17 -0300

    Fix maybe import

--- fpylib/functors/__init__.py
+++ fpylib/functors/__init__.py
@@ -1,5 +1,5 @@
 from fpylib.functors.functor import fmap
 from fpylib.functors.applicative import apply, lift_a2
 from fpylib.functors.monad import unit
-from fpylib.functors.maybe import *
+from fpylib.functors.maybe import Maybe, Just, Nothing
 from fpylib.functors.flist import *
