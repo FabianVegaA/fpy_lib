7c89e6d285a440f295c6423daddf867b33385a2a
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2021-11-25 12:56:21 -0300

    Fix import irange

--- fpylib/__init__.py
+++ fpylib/__init__.py
@@ -1,3 +1,4 @@
 from fpylib.composer import compose, parallelize
 from fpylib.lazyness import lazy_eval, lazy_class
+from fpylib.irange import irange, char_encode, char_decode, num_irange, char_irange
 from fpylib.functors import *
