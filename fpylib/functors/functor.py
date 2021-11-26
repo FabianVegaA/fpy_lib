a4706f05b9fc95ff5e727bbefddfcc1aea09ceec
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2021-11-25 22:34:11 -0300

    Add docs to methos

--- fpylib/functors/functor.py
+++ fpylib/functors/functor.py
@@ -6,7 +6,24 @@ S = TypeVar("S")
 
 
 class Functor(Generic[T]):
+    """
+    Class Functor is a generic class that implements the Functor type class.
+
+
+    :param value: The value of the functor.
+    :type value: T
+    :return: A functor with the given value.
+    """
+
     def __init__(self, value: Optional[T] = None) -> None:
+        """
+        Initialize a functor with the given value.
+
+        :param value: The value of the functor.
+        :type value: Optional[T]
+        :return: A functor with the given value.
+        """
+
         def frozen_setattr(cls, __name: str, __value: Any) -> None:
             raise AttributeError("This object is not modifiable")
 
@@ -14,9 +31,21 @@ class Functor(Generic[T]):
         object.__setattr__(self, "__setattr__", frozen_setattr)
 
     def get(self) -> T:
+        """
+        Get the value of the functor.
+
+        :return: The value of the functor.
+        """
         return self.__value
 
     def fmap(self, func: Callable[["Functor[T]"], "Functor[S]"]) -> "Functor[S]":
+        """
+        Apply a function to the value of the functor.
+
+        :param func: The function to apply.
+        :type func: Callable[["Functor[T]"], "Functor[S]"]
+        :return: A functor with the value of the functor after applying the function.
+        """
         return Functor(func(self.get()))
 
 
