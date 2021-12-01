2b9ff3f198f59c2455c4e9254c7fd6d06f1fb8a5
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2021-12-01 16:00:25 -0300

    Fix imports functors

--- fpylib/functors/__init__.py
+++
@@ -1,5 +0,0 @@
-from fpylib.functors.functor import fmap
-from fpylib.functors.applicative import apply, lift_a2
-from fpylib.functors.monad import unit
-from fpylib.functors.maybe import Maybe, Just, Nothing
-from fpylib.functors.flist import *
